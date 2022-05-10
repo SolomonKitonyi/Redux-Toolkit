d41e446585edb63b02bba1c830f23ed3d26101fa
SolomonKitonyi <kitonyisolomon@gmail.com>
2022-05-10 13:49:07 +0300

    add async actions

--- app/store.js
+++ app/store.js
@@ -5,12 +5,14 @@ const configureStore = require("@reduxjs/toolkit").configureStore;
 // const logger = reducerLogger.createLogger();
 
 const cakeReducer = require("../features/cake/cakeSlice");
-const icecreamReducer = require("../features/icecream/iceCreamSlice")
+const icecreamReducer = require("../features/icecream/iceCreamSlice");
+const userReducer = require("../features/user/userSlice");
 
 const store = configureStore({
     reducer: {
         cake: cakeReducer,
         icecream: icecreamReducer,
+        user: userReducer,
     },
     // middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger), 
 });
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
 const icecreamActions = require("./features/icecream/iceCreamSlice").icecreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log("initialState ", store.getState());
 
@@ -8,14 +9,16 @@ const unsubscribe = store.subscribe(() => {
     console.log("Updated state", store.getState())
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+store.dispatch(fetchUsers())
 
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(3));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
 
-unsubscribe();
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(3));
+
+// unsubscribe();
